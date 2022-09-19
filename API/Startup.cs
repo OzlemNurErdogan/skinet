22e96a261db4295c6158553c5387c372d2868aa8
OzlemNurErdogan <ozlemnurerdogan3@gmail.com>
2022-09-19 17:45:30 +0300

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -47,6 +47,13 @@ namespace API
 
             services.AddAplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt=>
+            {
+                opt.AddPolicy("CorsPolicy", policy=>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
            
 
@@ -63,6 +70,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
